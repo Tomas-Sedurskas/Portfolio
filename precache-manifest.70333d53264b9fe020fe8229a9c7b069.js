0629e657929515adab265ea07042ee7cd447ba79
Tomas <tomas.sedurskas@gmail.com>
2019-07-26 16:54:26 +0200

    Updates

---
+++ precache-manifest.70333d53264b9fe020fe8229a9c7b069.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "087a15822144dd25c4453d7a141b7f6e",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "bf6878714f6f40614378",
+    "url": "/Portfolio/static/css/main.50b8e7b4.chunk.css"
+  },
+  {
+    "revision": "6c69304018c45cba02c8",
+    "url": "/Portfolio/static/js/2.001acf8d.chunk.js"
+  },
+  {
+    "revision": "bf6878714f6f40614378",
+    "url": "/Portfolio/static/js/main.5df8c955.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "09e4ae887aff90dcc02ea6c67b787ba9",
+    "url": "/Portfolio/static/media/Apel-Desktop.09e4ae88.jpg"
+  },
+  {
+    "revision": "bb891939f99340f5c3c65aa5311c1e95",
+    "url": "/Portfolio/static/media/Apel-Mobile.bb891939.jpg"
+  },
+  {
+    "revision": "bc2c45190efa89e14a2fe7a7758d0875",
+    "url": "/Portfolio/static/media/Autolite-Desktop.bc2c4519.jpg"
+  },
+  {
+    "revision": "61c2447f2551409442c75cdef418d6db",
+    "url": "/Portfolio/static/media/Autolite-Logo.61c2447f.jpg"
+  },
+  {
+    "revision": "9d2897062b93e61a698f25c6ba0a7cc3",
+    "url": "/Portfolio/static/media/Autolite-Mobile.9d289706.jpg"
+  },
+  {
+    "revision": "e8323b821a7637153fe4b59a4afbe396",
+    "url": "/Portfolio/static/media/Dashy-Logo.e8323b82.jpg"
+  },
+  {
+    "revision": "f74045282d0f22f0cd49415b3125606f",
+    "url": "/Portfolio/static/media/Dashy-Main.f7404528.jpg"
+  },
+  {
+    "revision": "b9e7b4453168cda279b58628d0c1e3b3",
+    "url": "/Portfolio/static/media/Travel-Intro.b9e7b445.jpg"
+  },
+  {
+    "revision": "7708d7f070dcad69e27273b9847aea46",
+    "url": "/Portfolio/static/media/Travel-Logo.7708d7f0.jpg"
+  },
+  {
+    "revision": "f3b109d13cbbb29516c21e8909759029",
+    "url": "/Portfolio/static/media/Travel-Main.f3b109d1.jpg"
+  },
+  {
+    "revision": "0de09b66b6b205305d4ab9d13925439c",
+    "url": "/Portfolio/static/media/Travel-Onboarding.0de09b66.jpg"
+  },
+  {
+    "revision": "f039cfacad55631d6d38eafbf9229d40",
+    "url": "/Portfolio/static/media/X.f039cfac.svg"
+  }
+]);
